4978597b7d4c613e9ed16dd2ece88212390fe40b
Agus <agus.aguirre10@gmail.com>
2024-01-20 17:13:55 -0300

    Added score and score ui

---
+++ Assets/Managers/ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MiniJam150
+{
+	public class ScoreManager : MonoBehaviour
+	{
+		public int Score = 0;
+
+		private List<Miscelaneous.PlayerDetector> detectors;
+
+
+		public void PlayerDetected(Miscelaneous.PlayerDetector detector)
+			=> detectors.Add(detector);
+
+		public void PlayerLeft(Miscelaneous.PlayerDetector detector)
+		{
+			detectors.Remove(detector);
+			if (detectors.Count == 0)
+				delta = 0f;
+		}
+		
+
+		private void Awake()
+		{
+			detectors = new List<Miscelaneous.PlayerDetector>();
+		}
+
+		private float delta = 0f;
+		private void Update()
+		{
+			if (detectors.Count > 0)
+			{
+				delta += Time.deltaTime;
+				if (delta >= 1)
+				{
+					Score += 1;
+					delta = 0;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/UI/ScoreDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+namespace MiniJam150.UI
+{
+	public class ScoreDisplay : MonoBehaviour
+	{
+		[SerializeField] private ScoreManager scoreManager;
+		[SerializeField] private TextMeshProUGUI scoreDisplay;
+
+		private int m_prevScore = 0;
+		private void Update()
+		{
+			if (m_prevScore != scoreManager.Score)
+			{
+				m_prevScore = scoreManager.Score;
+				scoreDisplay.text = $"Score: {m_prevScore}";
+			}
+		}
+	}
+}
