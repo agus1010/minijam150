be36212ead0fe0e6da04009270ad23a04b23580d
Agus <agus.aguirre10@gmail.com>
2024-01-28 18:10:23 -0300

    Added sounds+

---
+++ Assets/Managers/CrowdSoundsManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+namespace MiniJam150
+{
+	public class CrowdSoundsManager : MonoBehaviour
+	{
+		[SerializeField] private AudioSource hubblingNoisesSource;
+		[SerializeField] private AudioSource introAplausesSource;
+		[SerializeField] private AudioSource singingAplausesSource;
+		[SerializeField] private AudioSource outroAplausesSource;
+
+		public void PlayApplauses()
+		{
+			singingAplausesSource.Play();
+		}
+
+		public void PlayEndGame()
+		{
+			outroAplausesSource.Play();
+		}
+
+		public void StartGame()
+		{
+			m_lowerVolume = true;
+		}
+
+
+
+		bool m_lowerVolume = false;
+		private void Update()
+		{
+			if (m_lowerVolume)
+			{
+				hubblingNoisesSource.volume -= Time.deltaTime;
+				if (hubblingNoisesSource.volume == 0)
+					m_lowerVolume = false;
+			}
+		}
+	}
+}
